a3c11c57217161b93f3392a0707f763a379c6ea4
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2023-11-27 19:18:35 +0100

    commit

---
+++ yassine_bouaquil_avito_v2/yassine_bouaquil_avito_v2/cart-user/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Ecommerce Site</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+    }
+
+    header {
+      background-color: #333;
+      color: #fff;
+      text-align: center;
+      padding: 1em 0;
+    }
+
+    section {
+      padding: 2em;
+      text-align: center;
+    }
+
+    button {
+      background-color: #4CAF50;
+      color: white;
+      padding: 10px 20px;
+      font-size: 16px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+
+    button:hover {
+      background-color: #45a049;
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>Your Ecommerce Site</h1>
+  </header>
+
+  <section>
+    <h2>Welcome to our store!</h2>
+    <p>Discover amazing products and shop with confidence.</p>
+    <button onclick="window.location.href='register.html'">Register</button>
+  </section>
+</body>
+</html>
---
+++ yassine_bouaquil_avito_v2/yassine_bouaquil_avito_v2/yassine_bouaquil_avito_v2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Ecommerce Site</title>
+  <link rel="stylesheet" href="css/style2.css">
+
+</head>
+<body>
+  <header>
+    <h1></h1>
+  </header>
+
+  <section>
+    <h2>Welcome to our store!</h2>
+    <p>Discover amazing products and shop with confidence.</p>
+    <button onclick="window.location.href='login.php'">Reger</button>
+  </section>
+</body>
+</html>
